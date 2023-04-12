ee25cebaf36a0c5a641ac8fd2552bec05a248356
xincheng-cao <36287551+xincheng-cao@users.noreply.github.com>
2023-04-12 17:10:37 +0800

    update tests/

--- tests/openai/te5t_openai_emb.py
+++ tests/openai/te5t_openai_emb.py
@@ -1,5 +1,5 @@
 import openai
 
 openai.api_key='sk-chaogeyangjinjiewuyuanqingchenconggejinxiaoluweng'
-response = openai.Embedding.create(input='derby innovation lab', model="text-embedding-ada-002")
+response = openai.Embedding.create(input=['derby innovation lab','chao yang','jinjie wu'], model="text-embedding-ada-002")
 print(response)
