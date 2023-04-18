334b88209314c88197dfa3b49c7f729ad345e6c7
shiba <36287551+xincheng-cao@users.noreply.github.com>
2023-04-18 08:51:19 +0800

    update scripts/process_json/

--- scripts/process_json/convert_csv2json.py
+++ scripts/process_json/convert_csv2json.py
@@ -42,7 +42,7 @@ out_list=[]
 for i in range(out_df.shape[0]):
     out_list.append(
         {
-            'id':str(out_df.iloc[i]['id']),
+            'id':str(out_df.iloc[i]['id'])+'_description',
             'text':out_df.iloc[i]['text'],
         }
     )
@@ -76,3 +76,62 @@ out_df:pd.DataFrame=df.apply(func=apply_fn4hotel_id_coord_ext,
 
 out_df.to_csv('sanya_coord.csv',sep='\x01',header=True,index=False,)
 
+
+
+
+
+
+# name+zone+area+address
+# features
+# honor_tags
+# bullet_screen_info
+# hotel_facilities_popular
+# hotel_facilities_full
+
+def apply_fn_0(sz:pd.Series):
+    temp={
+        'id': sz['hotel_id'],
+        'name+zone+area+address':sz['name']+'\n'+sz['zone']+'\n'+sz['area']+'\n'+sz['address'],
+        'features':sz['features'],
+        'honor_tags':sz['honor_tags'],
+        'bullet_screen_info':sz['bullet_screen_info'],
+        'hotel_facilities_popular':sz['hotel_facilities_popular'],
+        'hotel_facilities_full':sz['hotel_facilities_full'],
+    }
+    return pd.Series(temp)
+
+out_df:pd.DataFrame=df.apply(apply_fn_0,axis=1,)
+
+
+col_list=['name+zone+area+address',
+          'features',
+          'honor_tags',
+          'bullet_screen_info',
+          'hotel_facilities_popular',
+          'hotel_facilities_full',
+          ]
+for col in col_list:
+    out_list = []
+    for i in range(out_df.shape[0]):
+        out_list.append(
+            {
+                'id': str(out_df.iloc[i]['id']) + '_'+col,
+                'text': out_df.iloc[i][col],
+            }
+        )
+
+    with open(file='./sanya_{subname}.json'.format(subname=col), mode='w') as f:
+        f.write(
+            json.dumps(
+                out_list,
+                ensure_ascii=False,
+                indent=3,
+            )
+        )
+
+
+
+
+
+
+print('done')
